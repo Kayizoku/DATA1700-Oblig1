14f6be40ca44bf983923d568f9af4c57409f0bef
Mohammad Tayyab Khalid <tayyabkhalid306@gmail.com>
2020-02-15 10:23:36 +0100

    Oppgaven funker som den skal. Ferdig!!

--- src/main/resources/static/BestillingAvKinoBiletter.js
+++ src/main/resources/static/BestillingAvKinoBiletter.js
@@ -1,6 +1,5 @@
 let personRegister = [];
 
-
 function billettKjopt(){
     const antallBilletter = document.getElementById("antallBilletter").value;
     const valgtFilm = document.getElementById("valgtFilm").value;
@@ -19,22 +18,22 @@ function billettKjopt(){
     };
 
     personRegister.push(person);
-    console.log(personRegister);
 
-    //Sjekker om noe har blitt input i feltet og returnerer feil melding eller ei
-        if (antallBilletter === "" || fornavn === "" || etternavn === "" || telefonnr === "" || epost === "" ){
-            document.getElementById("feilAntallBiletter").innerHTML = "Ma skrive noe inn i antall!";
-            document.getElementById("feilFornavn").innerHTML = "Ma skrive noe inn i fornavnet!";
-            document.getElementById("feilEtternavn").innerHTML = "Ma skrive noe inn i etternavnet!";
-            document.getElementById("feilTelefonnr").innerHTML = "Ma skrive noe inn i telefonnr!!";
-            document.getElementById("feilEpost").innerHTML = "Ma skrive noe inn i epost!";
+    // input-validering som sjekker om noe har blitt tastet i feltet
+
+    if (antallBilletter === "" || fornavn === "" || etternavn === "" || telefonnr === "" || epost === "" ){
+        document.getElementById("feilAntallBiletter").innerHTML = "Ma skrive noe inn i antall!";
+        document.getElementById("feilFornavn").innerHTML = "Ma skrive noe inn i fornavnet!";
+        document.getElementById("feilEtternavn").innerHTML = "Ma skrive noe inn i etternavnet!";
+        document.getElementById("feilTelefonnr").innerHTML = "Ma skrive noe inn i telefonnr!";
+        document.getElementById("feilEpost").innerHTML = "Ma skrive noe inn i epost!";
     } else {
-            document.getElementById("feilAntallBiletter").innerHTML = "";
-            document.getElementById("feilFornavn").innerHTML = "";
-            document.getElementById("feilEtternavn").innerHTML = "";
-            document.getElementById("feilTelefonnr").innerHTML = "";
-            document.getElementById("feilEpost").innerHTML = "";
-        }
+        document.getElementById("feilAntallBiletter").innerHTML = "";
+        document.getElementById("feilFornavn").innerHTML = "";
+        document.getElementById("feilEtternavn").innerHTML = "";
+        document.getElementById("feilTelefonnr").innerHTML = "";
+        document.getElementById("feilEpost").innerHTML = "";
+    }
 
     document.getElementById("antallBilletter").value;
     document.getElementById("valgtFilm").value = "";
@@ -43,11 +42,10 @@ function billettKjopt(){
     document.getElementById("telefonnr").value = "";
     document.getElementById("epost").value = "";
 
-    let ut = "<table><tr>" +
-        "<th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th>" + "</tr>";
+    let ut = "<table><tr>" + "<th>Film</th><th>Antall</th><th>Fornavn</th><th>" +
+        "Etternavn</th><th>Telefonnr</th><th>Epost</th>" + "</tr>";
 
     for (let p of personRegister){
-        console.log(personRegister);
         ut+="<tr>";
         ut+="<td>"+p.valgtFilm+"<td>"+p.antallBilletter+"</td><td>"
             +p.fornavn+"</td><td>"+p.etternavn+"</td><td>p.telefonnr<td>p.epost</td>";
