588ad251047383a78d28b3e518e9da179590303d
Mohammad Tayyab Khalid <tayyabkhalid306@gmail.com>
2020-02-15 09:57:15 +0100

    Lagt til validering av input og litt css kode

--- src/main/resources/static/BestillingAvKinoBiletter.js
+++ src/main/resources/static/BestillingAvKinoBiletter.js
@@ -1,7 +1,7 @@
 let personRegister = [];
 
-function billettKjopt(){
 
+function billettKjopt(){
     const antallBilletter = document.getElementById("antallBilletter").value;
     const valgtFilm = document.getElementById("valgtFilm").value;
     const fornavn = document.getElementById("fornavn").value;
@@ -21,12 +21,22 @@ function billettKjopt(){
     personRegister.push(person);
     console.log(personRegister);
 
-    if (valgtFilm === "" || fornavn === "" || etternavn === "" || telefonnr === "" || epost === "" ){
-        document.getElementById("feilValgtFilm");
-        document.getElementById("feilFornavn").innerHTML = "påbudt med fornavn!";
-        document.getElementById("feilEtternavn").innerHTML = "Feil etternavn!";
-    }
+    //Sjekker om noe har blitt input i feltet og returnerer feil melding eller ei
+        if (antallBilletter === "" || fornavn === "" || etternavn === "" || telefonnr === "" || epost === "" ){
+            document.getElementById("feilAntallBiletter").innerHTML = "Ma skrive noe inn i antall!";
+            document.getElementById("feilFornavn").innerHTML = "Ma skrive noe inn i fornavnet!";
+            document.getElementById("feilEtternavn").innerHTML = "Ma skrive noe inn i etternavnet!";
+            document.getElementById("feilTelefonnr").innerHTML = "Ma skrive noe inn i telefonnr!!";
+            document.getElementById("feilEpost").innerHTML = "Ma skrive noe inn i epost!";
+    } else {
+            document.getElementById("feilAntallBiletter").innerHTML = "";
+            document.getElementById("feilFornavn").innerHTML = "";
+            document.getElementById("feilEtternavn").innerHTML = "";
+            document.getElementById("feilTelefonnr").innerHTML = "";
+            document.getElementById("feilEpost").innerHTML = "";
+        }
 
+    document.getElementById("antallBilletter").value;
     document.getElementById("valgtFilm").value = "";
     document.getElementById("fornavn").value = "";
     document.getElementById("etternavn").value = "";
@@ -34,23 +44,21 @@ function billettKjopt(){
     document.getElementById("epost").value = "";
 
     let ut = "<table><tr>" +
-        "<th>Film</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th>" + "</tr>";
+        "<th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th>" + "</tr>";
 
     for (let p of personRegister){
         console.log(personRegister);
         ut+="<tr>";
-        ut+="<td>"+p.valgtFilm+"<td>"+p.fornavn+"</td><td>"+p.etternavn+"</td><td>"+p.telefonnr+"</td><td>p.epost</td>";
+        ut+="<td>"+p.valgtFilm+"<td>"+p.antallBilletter+"</td><td>"
+            +p.fornavn+"</td><td>"+p.etternavn+"</td><td>p.telefonnr<td>p.epost</td>";
         ut+="</tr>";
 
         document.getElementById("visPersonRegister").innerHTML=ut;
     }
-
 }
 
 function slettBillettene() {
     personRegister = [];
     document.getElementById("visPersonRegister").innerHTML="";
-
-
 }
 
